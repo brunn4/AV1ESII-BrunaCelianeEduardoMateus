97fdb66a95b4ce86a0ebdf93133ae464ab51d843
brunn4 <bruna.silva11@aluno.ifce.edu.br>
2026-03-06 18:05:06 -0300

    Funções matemáticas

---
+++ index.js
@@ -0,0 +1,17 @@
+function potencia(a, b) {
+  return Math.pow(a, b);
+}
+
+function maior(a, b) {
+  return Math.max(a, b);
+}
+
+function menor(a, b) {
+  return Math.min(a, b);
+}
+
+function raiz(numero) {
+  return Math.sqrt(numero);
+}
+
+module.exports = { potencia, maior, menor, raiz };
